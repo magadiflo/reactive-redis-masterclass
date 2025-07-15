c034ceacabc761fdf035f238d1107780dca3717f
Martín Díaz <magadiflo@gmail.com>
2025-07-15 13:09:44 -0500

    Redisson Codec

--- projects/redisson-playground/src/test/java/dev/magadiflo/test/Lec01KeyValueTest.java
+++ projects/redisson-playground/src/test/java/dev/magadiflo/test/Lec01KeyValueTest.java
@@ -3,6 +3,7 @@ package dev.magadiflo.test;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.redisson.api.RBucketReactive;
+import org.redisson.client.codec.StringCodec;
 import reactor.core.publisher.Mono;
 import reactor.test.StepVerifier;
 
@@ -12,6 +13,18 @@ class Lec01KeyValueTest extends BaseTest {
     @Test
     void keyValueAccessTest() {
         RBucketReactive<String> bucket = this.client.getBucket("user:1:name");
+        Mono<Void> set = bucket.set("sam");
+        Mono<Void> get = bucket.get()
+                .doOnNext(s -> log.info("{}", s))
+                .then();
+
+        StepVerifier.create(set.concatWith(get))
+                .verifyComplete();
+    }
+
+    @Test
+    void keyValueAccessTestStringCodec() {
+        RBucketReactive<String> bucket = this.client.getBucket("user:1:name", StringCodec.INSTANCE);
         Mono<Void> set = bucket.set("sam");
         Mono<Void> get = bucket.get()
                 .doOnNext(s -> log.info("{}", s))
