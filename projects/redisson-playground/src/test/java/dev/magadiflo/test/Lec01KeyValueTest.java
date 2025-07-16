f8b1055df0352afa899d2852649ad8f627208966
Martín Díaz <magadiflo@gmail.com>
2025-07-16 11:20:15 -0500

    Bucket Expiry

--- projects/redisson-playground/src/test/java/dev/magadiflo/test/Lec01KeyValueTest.java
+++ projects/redisson-playground/src/test/java/dev/magadiflo/test/Lec01KeyValueTest.java
@@ -7,6 +7,8 @@ import org.redisson.client.codec.StringCodec;
 import reactor.core.publisher.Mono;
 import reactor.test.StepVerifier;
 
+import java.time.Duration;
+
 @Slf4j
 class Lec01KeyValueTest extends BaseTest {
 
@@ -31,6 +33,43 @@ class Lec01KeyValueTest extends BaseTest {
                 .then();
 
         StepVerifier.create(set.concatWith(get))
+                .verifyComplete();
+    }
+
+    @Test
+    void keyValueExpiryTest() {
+        RBucketReactive<String> bucket = this.client.getBucket("user:1:name", StringCodec.INSTANCE);
+        Mono<Void> set = bucket.set("sam", Duration.ofSeconds(10));
+        Mono<Void> get = bucket.get()
+                .doOnNext(s -> log.info("{}", s))
+                .then();
+
+        StepVerifier.create(set.concatWith(get))
+                .verifyComplete();
+    }
+
+    @Test
+    void keyValueExtendExpiryTest() {
+        RBucketReactive<String> bucket = this.client.getBucket("user:1:name", StringCodec.INSTANCE);
+        Mono<Void> set = bucket.set("sam", Duration.ofSeconds(10));
+        Mono<Void> get = bucket.get()
+                .doOnNext(s -> log.info("{}", s))
+                .then();
+        StepVerifier.create(set.concatWith(get))
+                .verifyComplete();
+
+        // extendiendo el tiempo de vida
+        this.sleep(5000);
+        Mono<Boolean> mono = bucket.expire(Duration.ofSeconds(60));
+        StepVerifier.create(mono)
+                .expectNext(true)
+                .verifyComplete();
+
+        // acceso al tiempo de expiración
+        Mono<Void> ttl = bucket.remainTimeToLive()
+                .doOnNext(time -> log.info("{}", time))
+                .then();
+        StepVerifier.create(ttl)
                 .verifyComplete();
     }
 }
