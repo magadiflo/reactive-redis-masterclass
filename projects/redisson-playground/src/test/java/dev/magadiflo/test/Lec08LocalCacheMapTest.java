d15d489777aaa53cda9391610ed737b0d03f47c6
Martín Díaz <magadiflo@gmail.com>
2025-07-18 13:07:14 -0500

    Local Cached Map: Sincronización de caché entre memoria local y Redis

---
+++ projects/redisson-playground/src/test/java/dev/magadiflo/test/Lec08LocalCacheMapTest.java
@@ -0,0 +1,37 @@
+package dev.magadiflo.test;
+
+import dev.magadiflo.test.config.RedissonConfig;
+import dev.magadiflo.test.dto.Student;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.redisson.api.RLocalCachedMap;
+import org.redisson.api.RedissonClient;
+import org.redisson.api.options.LocalCachedMapOptions;
+import org.redisson.codec.TypedJsonJacksonCodec;
+
+
+@Slf4j
+class Lec08LocalCacheMapTest extends BaseTest {
+
+    private RLocalCachedMap<Integer, Student> studentsMap;
+
+    @Override
+    @BeforeAll
+    void beforeAll() {
+        RedissonConfig redissonConfig = new RedissonConfig();
+        RedissonClient redissonClient = redissonConfig.getClient();
+
+        LocalCachedMapOptions<Integer, Student> mapOptions = LocalCachedMapOptions.<Integer, Student>name("students")
+                .codec(new TypedJsonJacksonCodec(Integer.class, Student.class))
+                .syncStrategy(LocalCachedMapOptions.SyncStrategy.UPDATE)
+                .reconnectionStrategy(LocalCachedMapOptions.ReconnectionStrategy.CLEAR);
+
+        this.studentsMap = redissonClient.getLocalCachedMap(mapOptions);
+    }
+
+    @Test
+    void mapCacheTest() {
+        // TODO. Implement later
+    }
+}
