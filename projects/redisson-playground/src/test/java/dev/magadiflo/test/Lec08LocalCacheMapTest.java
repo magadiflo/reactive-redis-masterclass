b72221e170916c8b5fda7a55b26e5ef01c0aed4e
Martín Díaz <magadiflo@gmail.com>
2025-07-21 13:22:58 -0500

    Local Cached Map - Parte 2: Pruebas de sincronización local

--- projects/redisson-playground/src/test/java/dev/magadiflo/test/Lec08LocalCacheMapTest.java
+++ projects/redisson-playground/src/test/java/dev/magadiflo/test/Lec08LocalCacheMapTest.java
@@ -9,6 +9,9 @@ import org.redisson.api.RLocalCachedMap;
 import org.redisson.api.RedissonClient;
 import org.redisson.api.options.LocalCachedMapOptions;
 import org.redisson.codec.TypedJsonJacksonCodec;
+import reactor.core.publisher.Flux;
+
+import java.time.Duration;
 
 
 @Slf4j
@@ -25,13 +28,31 @@ class Lec08LocalCacheMapTest extends BaseTest {
         LocalCachedMapOptions<Integer, Student> mapOptions = LocalCachedMapOptions.<Integer, Student>name("students")
                 .codec(new TypedJsonJacksonCodec(Integer.class, Student.class))
                 .syncStrategy(LocalCachedMapOptions.SyncStrategy.UPDATE)
-                .reconnectionStrategy(LocalCachedMapOptions.ReconnectionStrategy.CLEAR);
+                .reconnectionStrategy(LocalCachedMapOptions.ReconnectionStrategy.CLEAR)
+                .retryAttempts(3)                           // Número de reintentos
+                .retryInterval(Duration.ofMillis(1500));   // Intervalo entre reintentos
 
         this.studentsMap = redissonClient.getLocalCachedMap(mapOptions);
     }
 
     @Test
-    void mapCacheTest() {
-        // TODO. Implement later
+    void appServer1() {
+        Student student1 = new Student("Milagros", 19, "Lima");
+        Student student2 = new Student("Kiara", 24, "Rumisapa");
+
+        this.studentsMap.put(1, student1);
+        this.studentsMap.put(2, student2);
+
+        Flux.interval(Duration.ofSeconds(1))
+                .doOnNext(i -> log.info("{} => {}", i, this.studentsMap.get(1)))
+                .subscribe();
+
+        sleep(600_000);
+    }
+
+    @Test
+    void appServer2() {
+        Student student1 = new Student("Milagros Díaz", 19, "Lima/Callao");
+        this.studentsMap.put(1, student1);
     }
 }
