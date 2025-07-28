cef20d0b66b6dbd37c634cf45c7e3b4e0ff2ff9f
Martín Díaz <magadiflo@gmail.com>
2025-07-28 11:07:21 -0500

    Sección 05: Spring WebFlux Caching - Introducción

---
+++ projects/redis-spring/src/test/java/dev/magadiflo/redis_spring/RedisSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.magadiflo.redis_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedisSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
