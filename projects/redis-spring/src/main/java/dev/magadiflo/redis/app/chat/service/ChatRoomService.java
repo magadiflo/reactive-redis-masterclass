4638de7925141da9e994a9bcec5b2a2c91cee76a
Martín Díaz <magadiflo@gmail.com>
2025-09-10 12:49:33 -0500

    Chat Messages Broadcasting

--- projects/redis-spring/src/main/java/dev/magadiflo/redis/app/chat/service/ChatRoomService.java
+++ projects/redis-spring/src/main/java/dev/magadiflo/redis/app/chat/service/ChatRoomService.java
@@ -1,21 +1,44 @@
 package dev.magadiflo.redis.app.chat.service;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.redisson.api.RTopicReactive;
+import org.redisson.api.RedissonReactiveClient;
+import org.redisson.client.codec.StringCodec;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.socket.WebSocketHandler;
+import org.springframework.web.reactive.socket.WebSocketMessage;
 import org.springframework.web.reactive.socket.WebSocketSession;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @Slf4j
+@RequiredArgsConstructor
 @Service
 public class ChatRoomService implements WebSocketHandler {
+
+    private final RedissonReactiveClient client;
+
     @Override
     public Mono<Void> handle(WebSocketSession session) {
+        String room = "dummy";
+        RTopicReactive topic = this.client.getTopic(room, StringCodec.INSTANCE);
+
         //subscribe
-        session.receive();
+        Mono<Void> incomingFlux = session.receive()
+                .map(WebSocketMessage::getPayloadAsText)
+                .flatMap(topic::publish)
+                .doOnError(throwable -> log.error(throwable.getMessage()))
+                .doFinally(signalType -> log.info("Subscriber finally {}", signalType))
+                .then();
 
         //publisher
-        //session.send();
-        return null;
+        Flux<WebSocketMessage> outgoingFlux = topic.getMessages(String.class)
+                .map(session::textMessage)
+                .doOnError(throwable -> log.error(throwable.getMessage()))
+                .doFinally(signalType -> log.info("Publisher finally {}", signalType));
+
+        return session.send(outgoingFlux)
+                .and(incomingFlux);
     }
 }
